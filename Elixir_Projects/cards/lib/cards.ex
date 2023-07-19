6f975e9791b621a3e986f6652055c9468712d580
Sipho Ndlalane <siphonhata@gmail.com>
2023-07-19 15:12:53 +0200

    Writing Documentation for Card Projetc and providing examples

--- Elixir_Projects/cards/lib/cards.ex
+++ Elixir_Projects/cards/lib/cards.ex
@@ -1,19 +1,13 @@
 defmodule Cards do
+
   @moduledoc """
-  Documentation for `Cards`.
+    Provides methods for creating and handling a deck of cards
   """
 
+@doc """
+  Returns a list of string representing a deck of playing cards
+"""
   @spec create_deck :: list
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-
   def create_deck do
     values = ["Ace","Two","Three","Four","Five","Six","Seven"]
     suits = ["Spades","Clubs", "Hearts","Diamonds"]
@@ -38,6 +32,19 @@ defmodule Cards do
     Enum.member?(deck,card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
