1063f2326d8dd04e2f25f6ea491ebe0def330993
Sipho Ndlalane <siphonhata@gmail.com>
2023-07-19 13:48:57 +0200

    Writing and reading from a file

--- Elixir_Projects/cards/lib/cards.ex
+++ Elixir_Projects/cards/lib/cards.ex
@@ -50,10 +50,16 @@ defmodule Cards do
 
   #Reading file contents
   def load(filename) do
-    {status, binary} = File.read(filename)
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "File does not exist";
+
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "File does not exist";
     end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck()
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
   end
 end
