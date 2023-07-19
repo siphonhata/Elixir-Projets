5e8a90dfbedef3d5257799f6f391d09f6abe595b
Sipho Ndlalane <siphonhata@gmail.com>
2023-07-19 16:12:39 +0200

    Doctest and testing of my projects

--- Elixir_Projects/cards/lib/cards.ex
+++ Elixir_Projects/cards/lib/cards.ex
@@ -4,12 +4,30 @@ defmodule Cards do
     Provides methods for creating and handling a deck of cards
   """
 
-@doc """
-  Returns a list of string representing a deck of playing cards
-"""
+  @doc """
+    Returns a list of string representing a deck of playing cards
+
+  ## Examples
+
+      iex(1)> Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+      "Five of Spades", "Six of Spades", "Seven of Spades", "Eight of Spades",
+      "Nine of Spades", "Ten of Spades", "Jack of Spades", "Queen of Spades",
+      "King of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+      "Four of Clubs", "Five of Clubs", "Six of Clubs", "Seven of Clubs",
+      "Eight of Clubs", "Nine of Clubs", "Ten of Clubs", "Jack of Clubs",
+      "Queen of Clubs", "King of Clubs", "Ace of Hearts", "Two of Hearts",
+      "Three of Hearts", "Four of Hearts", "Five of Hearts", "Six of Hearts",
+      "Seven of Hearts", "Eight of Hearts", "Nine of Hearts", "Ten of Hearts",
+      "Jack of Hearts", "Queen of Hearts", "King of Hearts", "Ace of Diamonds",
+      "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds",
+      "Six of Diamonds", "Seven of Diamonds", "Eight of Diamonds",
+      "Nine of Diamonds", "Ten of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds"]
+
+  """
 
   def create_deck do
-    values = ["Ace","Two","Three","Four","Five","Six","Seven"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades","Clubs", "Hearts","Diamonds"]
 
     # cards = for value <- values do
@@ -24,10 +42,22 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Shuffles a deck of cards and randomly place them
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card.
+
+  ## Examples
+      iex> deck = Cards.create_deck()
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck,card)
   end
@@ -48,6 +78,7 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
+
 
   #Saving Contents to a file
   def save(deck, filename) do
--- Elixir_Projects/cards/test/cards_test.exs
+++ Elixir_Projects/cards/test/cards_test.exs
@@ -9,4 +9,21 @@ defmodule CardsTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck())
+    assert deck_length == 52
+  end
+
+  test "deal func" do
+    deck = Cards.create_deck()
+    {hand, deck} = Cards.deal(deck, 1)
+    assert hand == ["Ace of Spades"]
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck()
+    refute deck == Cards.shuffle(deck)
+    assert deck != Cards.shuffle(deck)
+  end
 end
