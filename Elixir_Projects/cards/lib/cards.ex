7310b45fadf86d9f9f7653180f289c44e9e2390c
Sipho Ndlalane <siphonhata@gmail.com>
2023-07-20 10:04:28 +0200

    Complete project with doctests

--- Elixir_Projects/cards/lib/cards.ex
+++ Elixir_Projects/cards/lib/cards.ex
@@ -4,6 +4,8 @@ defmodule Cards do
     Provides methods for creating and handling a deck of cards
   """
 
+
+
   @doc """
     Returns a list of string representing a deck of playing cards
 
@@ -12,22 +14,19 @@ defmodule Cards do
       iex(1)> Cards.create_deck
       ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
       "Five of Spades", "Six of Spades", "Seven of Spades", "Eight of Spades",
-      "Nine of Spades", "Ten of Spades", "Jack of Spades", "Queen of Spades",
-      "King of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
-      "Four of Clubs", "Five of Clubs", "Six of Clubs", "Seven of Clubs",
-      "Eight of Clubs", "Nine of Clubs", "Ten of Clubs", "Jack of Clubs",
-      "Queen of Clubs", "King of Clubs", "Ace of Hearts", "Two of Hearts",
-      "Three of Hearts", "Four of Hearts", "Five of Hearts", "Six of Hearts",
-      "Seven of Hearts", "Eight of Hearts", "Nine of Hearts", "Ten of Hearts",
-      "Jack of Hearts", "Queen of Hearts", "King of Hearts", "Ace of Diamonds",
-      "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds",
-      "Six of Diamonds", "Seven of Diamonds", "Eight of Diamonds",
-      "Nine of Diamonds", "Ten of Diamonds", "Jack of Diamonds", "Queen of Diamonds", "King of Diamonds"]
+      "Nine of Spades", "Ten of Spades", "Ace of Clubs", "Two of Clubs",
+      "Three of Clubs", "Four of Clubs", "Five of Clubs", "Six of Clubs",
+      "Seven of Clubs", "Eight of Clubs", "Nine of Clubs", "Ten of Clubs",
+      "Ace of Hearts", "Two of Hearts","Three of Hearts", "Four of Hearts",
+      "Five of Hearts", "Six of Hearts", "Seven of Hearts", "Eight of Hearts",
+      "Nine of Hearts", "Ten of Hearts", "Ace of Diamonds", "Two of Diamonds",
+      "Three of Diamonds", "Four of Diamonds", "Five of Diamonds", "Six of Diamonds",
+      "Seven of Diamonds", "Eight of Diamonds", "Nine of Diamonds", "Ten of Diamonds"]
 
   """
 
   def create_deck do
-    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten"]
     suits = ["Spades","Clubs", "Hearts","Diamonds"]
 
     # cards = for value <- values do
@@ -42,12 +41,16 @@ defmodule Cards do
     end
   end
 
-  @doc """
-    Shuffles a deck of cards and randomly place them
-  """
-  def shuffle(deck) do
-    Enum.shuffle(deck)
-  end
+
+
+  # @doc """
+  #   Shuffles a deck of cards and randomly place them
+  # """
+  # def shuffle(deck) do
+  #   Enum.shuffle(deck)
+  # end
+
+
 
   @doc """
     Determines whether a deck contains a given card.
@@ -73,18 +76,54 @@ defmodule Cards do
       iex> {hand, deck} = Cards.deal(deck, 1)
       iex> hand
       ["Ace of Spades"]
+      iex> length(deck)
+      39
 
   """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc"""
+    Recieves a deck of cards and a filename.
+    Saves the cards on the deck inside the file using the erlang `term_to_binary/1`
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> Cards.save(deck, "my_deck")
+      :ok
+
+  """
 
   #Saving Contents to a file
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
+
+
+  @doc"""
+    Recieves the filename.
+    Converts the binary values to Lists of Strings, and if file is not found
+    an error message is shown.
+
+  ## Examples
+
+      iex> Cards.load("my_deck")
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+      "Five of Spades", "Six of Spades", "Seven of Spades", "Eight of Spades",
+      "Nine of Spades", "Ten of Spades", "Ace of Clubs", "Two of Clubs",
+      "Three of Clubs", "Four of Clubs", "Five of Clubs", "Six of Clubs",
+      "Seven of Clubs", "Eight of Clubs", "Nine of Clubs", "Ten of Clubs",
+      "Ace of Hearts", "Two of Hearts", "Three of Hearts", "Four of Hearts",
+      "Five of Hearts", "Six of Hearts", "Seven of Hearts", "Eight of Hearts",
+      "Nine of Hearts", "Ten of Hearts", "Ace of Diamonds", "Two of Diamonds",
+      "Three of Diamonds", "Four of Diamonds", "Five of Diamonds", "Six of Diamonds",
+      "Seven of Diamonds", "Eight of Diamonds", "Nine of Diamonds",
+      "Ten of Diamonds"]
+
+  """
 
   #Reading file contents
   def load(filename) do
@@ -95,9 +134,30 @@ defmodule Cards do
     end
   end
 
+  # @doc """
+  #   Creates a deck of cards then divides a deck into a hand
+  #   and the remainder of the deck.
+
+  # ## Examples
+
+  #     iex> Cards.create_hand(5)
+  #     {["Eight of Hearts", "Three of Spades", "Four of Hearts", "Nine of Hearts",
+  #     "Five of Clubs"], ["Two of Spades", "Four of Diamonds", "Nine of Clubs",
+  #     "Two of Clubs", "Six of Hearts", "Five of Spades", "Four of Clubs",
+  #     "Three of Clubs", "Seven of Clubs", "Ten of Spades", "Six of Diamonds",
+  #     "Three of Diamonds", "Nine of Diamonds", "Three of Hearts", "Ace of Clubs",
+  #     "Two of Hearts", "Five of Diamonds", "Four of Spades", "Eight of Diamonds",
+  #     "Eight of Clubs", "Ace of Hearts", "Ace of Spades", "Ten of Diamonds",
+  #     "Seven of Spades","Seven of Diamonds", "Five of Hearts", "Two of Diamonds",
+  #     "Ten of Hearts","Eight of Spades", "Ace of Diamonds", "Six of Clubs",
+  #     "Nine of Spades","Ten of Clubs", "Six of Spades", "Seven of Hearts"]}
+
+  # """
+
   def create_hand(hand_size) do
     Cards.create_deck()
-    |> Cards.shuffle()
+    |> Enum.shuffle()
+    #|> Cards.shuffle() Alternative way
     |> Cards.deal(hand_size)
   end
 end
--- Elixir_Projects/cards/test/cards_test.exs
+++ Elixir_Projects/cards/test/cards_test.exs
@@ -6,24 +6,33 @@ defmodule CardsTest do
   #   assert Cards.hello() == :world
   # end
 
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
+  # test "the truth" do
+  #   assert 1 + 1 == 2
+  # end
 
-  test "create_deck makes 20 cards" do
+  test "create_deck makes 40 cards" do
     deck_length = length(Cards.create_deck())
-    assert deck_length == 52
+    assert deck_length == 40
   end
 
   test "deal func" do
     deck = Cards.create_deck()
     {hand, deck} = Cards.deal(deck, 1)
     assert hand == ["Ace of Spades"]
+    assert length(deck) == 39
   end
 
   test "shuffling a deck randomizes it" do
     deck = Cards.create_deck()
-    refute deck == Cards.shuffle(deck)
-    assert deck != Cards.shuffle(deck)
+    refute deck == Enum.shuffle(deck)
+    #refute deck == Cards.shuffle(deck)
+    #assert deck != Enum.shuffle(deck)
+    #assert deck != Cards.shuffle(deck)
+  end
+
+  test "Saving deck to a file" do
+    deck = Cards.create_deck()
+    ans = Cards.save(deck, "my_deck")
+    assert ans == :ok
   end
 end
